4f433742fb38592296bf165dbeda7ef72edb6320
kemal576 <kemal.sahin98@hotmail.com>
2025-07-17 01:08:10 +0300

    replace mongo ef core with mongo driver & add swagger

--- ProductWrite/Domain/Base/BaseEntity.cs
+++ ProductWrite/Domain/Base/BaseEntity.cs
@@ -1,11 +1,16 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace ProductWrite.Domain.Base;
 
 public class BaseEntity
 {
     [BsonId]
-    public Guid Id { get; init; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; protected init; } = Guid.NewGuid();
+
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+
+    public void TouchUpdatedAt() => UpdatedAt = DateTime.UtcNow;
 }
--- ProductWrite/Program.cs
+++ ProductWrite/Program.cs
@@ -1,39 +1,49 @@
 using FluentValidation;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using ProductWrite.Api;
 using ProductWrite.Api.Handlers;
 using ProductWrite.Application.Commands;
-using ProductWrite.Domain.Base;
 using ProductWrite.Infrastructure;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var mongoSettings = builder.Configuration.GetSection("MongoSettings").Get<MongoSettings>();
 if (mongoSettings == null)
     throw new ArgumentNullException(nameof(mongoSettings));
 
-// Service Registrations
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMongoDB(mongoSettings.ConnectionString, mongoSettings.DatabaseName));
+// Mongo Registration
+builder.Services.Configure<MongoSettings>(
+    builder.Configuration.GetSection("MongoSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(settings.DatabaseName);
+});
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddValidatorsFromAssemblyContaining<CreateProductCommand>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IUnitOfWork, ApplicationUnitOfWork>();
-
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapProductEndpoints(); // handler registration
-app.UseHttpsRedirection();
-
 app.Run();
