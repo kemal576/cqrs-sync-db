57d85244c725b1b57df26e328ba06deb4f1310c6
kemal576 <kemal.sahin98@hotmail.com>
2025-07-28 22:44:34 +0300

    add read repository

---
+++ ProductRead/models/product.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Product struct {
+	Id          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+}
---
+++ ProductRead/repositories/product_read_repository.go
@@ -0,0 +1,81 @@
+package repositories
+
+import (
+	"ProductRead/models"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+type ProductReadRepository struct {
+	es *elasticsearch.Client
+}
+
+func NewProductReadRepository(es *elasticsearch.Client) *ProductReadRepository {
+	return &ProductReadRepository{es: es}
+}
+
+func (r *ProductReadRepository) GetById(ctx context.Context, id string) (*models.Product, error) {
+	res, err := r.es.Get("products", id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	if res.IsError() {
+		return nil, fmt.Errorf("Error getting product: %s", res.String())
+	}
+
+	var doc struct {
+		Source models.Product `json:"_source"`
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&doc); err != nil {
+		return nil, err
+	}
+
+	return &doc.Source, nil
+}
+
+func (r *ProductReadRepository) GetAll(ctx context.Context) ([]models.Product, error) {
+	var products []models.Product
+
+	query := `{"query": {"match_all": {}}}`
+
+	res, err := r.es.Search(
+		r.es.Search.WithContext(ctx),
+		r.es.Search.WithIndex("products"),
+		r.es.Search.WithBody(strings.NewReader(query)),
+		r.es.Search.WithTrackTotalHits(true),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	if res.IsError() {
+		return nil, fmt.Errorf("Error searching products: %s", res.String())
+	}
+
+	var resp struct {
+		Hits struct {
+			Hits []struct {
+				Source models.Product `json:"_source"`
+			} `json:"hits"`
+		} `json:"hits"`
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return nil, err
+	}
+
+	for _, hit := range resp.Hits.Hits {
+		products = append(products, hit.Source)
+	}
+
+	return products, nil
+}
