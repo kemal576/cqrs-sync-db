2f6a896c8bb33fbfe1be06926737ffe92050c07c
kemal576 <kemal.sahin98@hotmail.com>
2025-08-01 01:57:57 +0300

    implement product consumer

--- ProductRead/main.go
+++ ProductRead/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ProductRead/consumers"
 	"ProductRead/handlers"
 	"ProductRead/repositories"
 	"os"
@@ -32,4 +33,10 @@ func main() {
 	r.GET("/products/:id", handlers.GetProductById(repo))
 
 	r.Run() // listens on :8080 by default
+
+	consumer, err := consumers.NewProductConsumer(os.Getenv("KAFKA_BROKER"), os.Getenv("PRODUCT_CDC_TOPIC"), esAddr)
+	if err != nil {
+		panic(err)
+	}
+	go consumer.Consume()
 }
